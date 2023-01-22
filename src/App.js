811d8783c77b557fd86d92a3e25c37129bc69a15
aj-sin <ajitcoder35@gmail.com>
2023-01-22 15:20:06 +0530

    Added progress bar and now its time to change class based components to function based components using hooks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar'
 import About from './components/About'
 import React, { Component } from 'react'
 import News from './components/News';
+import LoadingBar from 'react-top-loading-bar'
+
 import {
   BrowserRouter as Router,
   Route,
@@ -13,25 +15,35 @@ import {
 export default class App extends Component {
    pagesize=9
    country="in"
+  state={
+    progress:0
+  }
+  setProgress=(progress)=>{
+    this.setState({progress:progress})
+  }
+
   render() {
     return (
       <>
       <Router>
       <Navbar/>
+      <LoadingBar
+      height={3}
+        color='#72fc79'
+        progress={this.state.progress}
+        // onLoaderFinished={() => this.setProgress(0)}
+      />
 
       <Routes>
-
-
-
             <Route exact path="/about" element={<About key ='about'/>} />
-            <Route exact path="/" element={<News key ='general'  pagesize={this.pagesize} category={'general'} country={this.country}/>} />
-            <Route exact path="/business" element={<News key ='business'  pagesize={this.pagesize} category={'business'} country={this.country}/>} />
-            <Route exact path="/entertainment" element={<News key ='entertainment'  pagesize={this.pagesize} category={'entertainment'} country={this.country}/>} />
-            <Route exact path="/general" element={<News key ='general'  pagesize={this.pagesize} category={'general'} country={this.country}/>} />
-            <Route exact path="/health" element={<News key ='health'  pagesize={this.pagesize} category={'health'} country={this.country}/>} />
-            <Route exact path="/science" element={<News  key ='science' pagesize={this.pagesize} category={'science'} country={this.country}/>} />
-            <Route exact path="/sports" element={<News key ='sports'  pagesize={this.pagesize} category={'sports'} country={this.country}/>} />
-            <Route exact path="/technology" element={<News  key ='technology' pagesize={this.pagesize} category={'technology'} country={this.country}/>} />
+            <Route exact path="/" element={<News setProgress={this.setProgress} key ='general'  pagesize={this.pagesize} category={'general'} country={this.country}/>} />
+            <Route exact path="/business" element={<News setProgress={this.setProgress} key ='business'  pagesize={this.pagesize} category={'business'} country={this.country}/>} />
+            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} key ='entertainment'  pagesize={this.pagesize} category={'entertainment'} country={this.country}/>} />
+            <Route exact path="/general" element={<News setProgress={this.setProgress} key ='general'  pagesize={this.pagesize} category={'general'} country={this.country}/>} />
+            <Route exact path="/health" element={<News setProgress={this.setProgress} key ='health'  pagesize={this.pagesize} category={'health'} country={this.country}/>} />
+            <Route exact path="/science" element={<News setProgress={this.setProgress}  key ='science' pagesize={this.pagesize} category={'science'} country={this.country}/>} />
+            <Route exact path="/sports" element={<News setProgress={this.setProgress} key ='sports'  pagesize={this.pagesize} category={'sports'} country={this.country}/>} />
+            <Route exact path="/technology" element={<News setProgress={this.setProgress}  key ='technology' pagesize={this.pagesize} category={'technology'} country={this.country}/>} />
 
 
           </Routes>
--- src/components/News.js
+++ src/components/News.js
@@ -7,13 +7,13 @@ import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             article: [],
             loading: false,
             page: 1,
-            totalResults:0,
+            totalResults: 0,
         }
     }
 
@@ -25,44 +25,51 @@ export class News extends Component {
 
 
     updatenews = async () => {
+        this.props.setProgress(10)
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=049e0196f4584429ae9a41b7da6574d5&page=${this.state.page}&pagesize=${this.props.pagesize}`
         this.setState({ loading: true })
+        this.props.setProgress(30)
         let data = await fetch(url)
         let parsedata = await data.json()
         this.setState({ loading: false })
+        this.props.setProgress(90)
 
         this.setState({
 
             article: parsedata.articles,
             totalResults: parsedata.totalResults
         })
+        this.props.setProgress(100)
     }
 
     //this is important because it load the initial page of the website
     async componentDidMount() {
         this.updatenews()
+        this.setState({
+            page: this.state.page + 1
+        })
 
     }
 
-     fetchMoreData=async()=>{
-      this.setState({
-        page: this.state.page+1
-      })
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=049e0196f4584429ae9a41b7da6574d5&page=${this.state.page}&pagesize=${this.props.pagesize}`
+    fetchMoreData = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=049e0196f4584429ae9a41b7da6574d5&page=${this.state.page}&pagesize=${this.props.pagesize}`
         this.setState({ loading: true })
+
         let data = await fetch(url)
         let parsedata = await data.json()
 
         this.setState({
 
             article: this.state.article.concat(parsedata.articles),
-            loading:false
+            loading: false,
+            page: this.state.page + 1
         })
+
     }
 
 
 
-    
+
     render() {
 
         return (
@@ -72,24 +79,24 @@ export class News extends Component {
                 <InfiniteScroll
                     dataLength={this.state.article.length}
                     next={this.fetchMoreData}
-                    hasMore={this.state.article.length!==this.state.totalResults}
-                    loader={this.state.loading?<Spinner/>:true}
+                    hasMore={this.state.article.length !== this.state.totalResults}
+                    loader={this.state.loading ? <Spinner /> : true}
                 >
                     <div className="container">
-                        
-                    {<div className="row">
-                    {this.state.loading && <Spinner/>}
-                        {this.state.article.map((element) => {
-                            
-                            return <div className="col-md-4" key={element.url}>
-                                <NewsItem title={element.title} discription={element.description}
-                                    imageurl={element.urlToImage ? element.urlToImage : logo1} newsurl={element.url} author={element.author ? element.author : "Unkown"} Time={element.publishedAt} source={element.source.name} />
 
-                            </div>
-                        })}
+                        {<div className="row">
+                            {this.state.loading && <Spinner />}
+                            {this.state.article.map((element) => {
 
-                    </div>}
-                        </div>
+                                return <div className="col-md-4" key={element.url}>
+                                    <NewsItem title={element.title} discription={element.description}
+                                        imageurl={element.urlToImage ? element.urlToImage : logo1} newsurl={element.url} author={element.author ? element.author : "Unkown"} Time={element.publishedAt} source={element.source.name} />
+
+                                </div>
+                            })}
+
+                        </div>}
+                    </div>
                 </InfiniteScroll>
 
 
